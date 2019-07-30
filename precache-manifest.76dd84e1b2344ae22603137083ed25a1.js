08781c0e4b686fae859f0f696e254c00a0d328a0
Tran Quoc Khanh <khanhtran.cse@gmail.com>
2019-07-30 14:36:29 +0700

    Update build

---
+++ precache-manifest.76dd84e1b2344ae22603137083ed25a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "/admin/static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "7c4f45329e47798217a3",
+    "url": "/admin/static/js/runtime~main.b3695f6e.js"
+  },
+  {
+    "revision": "ac2f752f8913a186edf0",
+    "url": "/admin/static/js/main.ade5ee0f.chunk.js"
+  },
+  {
+    "revision": "977f1f7ae0f0dcb47bdb",
+    "url": "/admin/static/js/2.493a56a1.chunk.js"
+  },
+  {
+    "revision": "ac2f752f8913a186edf0",
+    "url": "/admin/static/css/main.b71f83f6.chunk.css"
+  },
+  {
+    "revision": "977f1f7ae0f0dcb47bdb",
+    "url": "/admin/static/css/2.b9dcd0fa.chunk.css"
+  },
+  {
+    "revision": "b6b852b2a6992af36bf5c0d41d02cec5",
+    "url": "/admin/index.html"
+  }
+];
