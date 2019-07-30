8ef669a268cb2795e0b24e1914a1393ea5b56ecf
Tran Quoc Khanh <khanhtran.cse@gmail.com>
2019-07-30 18:13:58 +0700

    rebuild

---
+++ precache-manifest.69b7683cd0d5c2607644cceb03dc3a8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "/my-viet-nam-admin/static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "8ed3d0ad0d0fa7928437",
+    "url": "/my-viet-nam-admin/static/js/runtime~main.bda705f2.js"
+  },
+  {
+    "revision": "b34774eba7a71b36eddb",
+    "url": "/my-viet-nam-admin/static/js/main.29074452.chunk.js"
+  },
+  {
+    "revision": "fa6f0ea299dbdc68c71c",
+    "url": "/my-viet-nam-admin/static/js/2.2f15fe0a.chunk.js"
+  },
+  {
+    "revision": "b34774eba7a71b36eddb",
+    "url": "/my-viet-nam-admin/static/css/main.b71f83f6.chunk.css"
+  },
+  {
+    "revision": "fa6f0ea299dbdc68c71c",
+    "url": "/my-viet-nam-admin/static/css/2.b9dcd0fa.chunk.css"
+  },
+  {
+    "revision": "f37f38cdbd15b4d17304e9c13243f3c1",
+    "url": "/my-viet-nam-admin/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-viet-nam-admin/precache-manifest.54d308cc6341d92518466784b49f2296.js"
+  "/my-viet-nam-admin/precache-manifest.69b7683cd0d5c2607644cceb03dc3a8a.js"
 );
 
 workbox.clientsClaim();
