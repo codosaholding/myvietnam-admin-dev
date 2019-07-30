e9fbb0cf394832262d36560178e7fc9a75e69ea9
Tran Quoc Khanh <khanhtran.cse@gmail.com>
2019-07-30 17:48:39 +0700

    update build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-viet-nam-admin/precache-manifest.6b90afe84d738baca0cd4baae9d01eb8.js"
+  "/my-viet-nam-admin/precache-manifest.8954dccd625ea64508fde6aafe83373c.js"
 );
 
 workbox.clientsClaim();
