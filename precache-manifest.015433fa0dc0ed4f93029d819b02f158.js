37bd89de65e7ee1e7a3c89c699be595c1a9578e1
Duong Vong <duongvongmt02@gmail.com>
2019-08-12 17:24:04 +0700

    update employees, transaction, refund, transfer, payment

---
+++ precache-manifest.015433fa0dc0ed4f93029d819b02f158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "/my-viet-nam-admin/static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "8ed3d0ad0d0fa7928437",
+    "url": "/my-viet-nam-admin/static/js/runtime~main.bda705f2.js"
+  },
+  {
+    "revision": "e6c66d0867a80622ab39",
+    "url": "/my-viet-nam-admin/static/js/main.b7e9b9e1.chunk.js"
+  },
+  {
+    "revision": "3d48b9d8f6ca928b4687",
+    "url": "/my-viet-nam-admin/static/js/2.d7a5cc74.chunk.js"
+  },
+  {
+    "revision": "e6c66d0867a80622ab39",
+    "url": "/my-viet-nam-admin/static/css/main.36494774.chunk.css"
+  },
+  {
+    "revision": "3d48b9d8f6ca928b4687",
+    "url": "/my-viet-nam-admin/static/css/2.61921c13.chunk.css"
+  },
+  {
+    "revision": "d6f3803c2e71cb0183f19fac23d646ad",
+    "url": "/my-viet-nam-admin/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-viet-nam-admin/precache-manifest.eb73d97c9971c46264964422c5e72aa0.js"
+  "/my-viet-nam-admin/precache-manifest.015433fa0dc0ed4f93029d819b02f158.js"
 );
 
 workbox.clientsClaim();
